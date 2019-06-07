ac8ec783f6ff5973ed876d682e7dbd98471d4efe
JF T <erion_lampusa@yahoo.fr>
2019-06-07 23:13:20 +0200

    Redéfinition des attributs

--- Item.py
+++ Item.py
@@ -1,6 +1,3 @@
-from datetime import datetime
-
-
 class Item:
 
     # Attributes
@@ -11,23 +8,11 @@ class Item:
     state = 0
     date_expected = None
     date_delivery = None
-    __date_creation = None
-    __date_modification = None
+    date_creation = None
+    date_modification = None
     __todo = None
 
     # Constructor
     def __init__(self, todo):
         self.todo = todo
-        todo.add_item(self)
-
-    # Methods
-    def before_save(self):
-        now = datetime.now()
-
-        if self.__date_creation is None:
-            # Creation
-            self.__date_creation = now
-        else:
-            # Modification
-            self.__date_modification = now
-
+        todo.items.append(self)
--- Todo.py
+++ Todo.py
@@ -1,26 +1,10 @@
-from datetime import datetime
-
-
 class Todo:
 
     # Attributes
     id = 0
     label = ""
     description = ""
-    __date_creation = None
-    __date_modification = None
-    __items = []
+    date_creation = None
+    date_modification = None
+    items = []
 
-    # Methods
-    def add_item(self, item):
-        self.__items.append(item)
-
-    def before_save(self):
-        now = datetime.now()
-
-        if self.__date_creation is None:
-            # Creation
-            self.__date_creation = now
-        else:
-            # Modification
-            self.__date_modification = now
