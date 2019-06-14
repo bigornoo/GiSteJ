ad41add219db517528ae13f716a98b4f2d604527
Jean-François Tronche <jftronche@univ-orleans.fr>
2019-06-14 23:13:40 +0200

    Merge branch 'master' of /Users/jftronche/Documents/Perso/code/python/GiSteJ with conflicts.

--- Test.py
+++ Test.py
@@ -5,3 +5,7 @@ from Item import Item
 todo_de_test = Todo()
 
 item1 = Item(todo_de_test)
+
+print(todo_de_test)
+
+
---
+++ test_jf.py
@@ -0,0 +1,7 @@
+from Todo import Todo
+from Item import Item
+
+le_tofu_ca_pue = Todo()
+
+item1 = Item(le_tofu_ca_pue)
+
